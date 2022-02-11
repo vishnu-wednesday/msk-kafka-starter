4a00e216fe285f2e2f7653e3c96de51227691321
vishnu-wednesday <vishnu@wednesday.is>
2022-02-11 20:40:01 +0530

    initial commit

---
+++ webpack.config.cjs
@@ -0,0 +1,16 @@
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+	entry: slsw.lib.entries,
+	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+	target: 'node',
+	externals: [nodeExternals()], // exclude external modules
+	output: {
+		library: {
+			type: 'module',
+			path: path.join(__dirname, 'dist'),
+			filename: '[name].js',
+		},
+	},
+};
